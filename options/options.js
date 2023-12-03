f686edc2d7886054e800b8f644b85559d0139b1c
Crowdbotics <team@crowdbotics.com>
2023-12-03 13:48:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "provinces-countries-44851",
+  url: "https://provinces-countries-44851.botics.co",
+  api: "https://provinces-countries-44851.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
